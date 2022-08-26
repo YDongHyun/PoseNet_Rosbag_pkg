a4efff08e290f4c6e606cb67f61eead8f278a715
양동현 <80799025+YDongHyun@users.noreply.github.com>
2022-08-26 17:23:20 +0900

    Add files via upload

--- LeGO-LOAM/src/sixdof_sub.cpp
+++ LeGO-LOAM/src/sixdof_sub.cpp
@@ -1,17 +1,23 @@
 #include "utility.h"
-
-int sixdof[7];
+#include <fstream>
+float sixdof[7];
+std::ofstream fout;
 
 void sixdof_sub(const nav_msgs::Odometry::ConstPtr& laserOdometry2){
   
-  sixdof[0]= laserOdometry2->pose.pose.position.x;
-  sixdof[1]= laserOdometry2->pose.pose.position.y;
-  sixdof[2]= laserOdometry2->pose.pose.position.z;
+  sixdof[0]= laserOdometry2->pose.pose.position.z;
+  sixdof[1]= laserOdometry2->pose.pose.position.x;
+  sixdof[2]= laserOdometry2->pose.pose.position.y;
   sixdof[3]= laserOdometry2->pose.pose.orientation.x;
   sixdof[4]= laserOdometry2->pose.pose.orientation.y;
   sixdof[5]= laserOdometry2->pose.pose.orientation.z;
-       
-  printf("x=%f, y=%f, z=%f, x=%f, y=%f, z=%f",sixdof[0],sixdof[1],sixdof[2],sixdof[3],sixdof[4],sixdof[5]);
+  sixdof[6]= laserOdometry2->pose.pose.orientation.w;
+  ofstream fout("train.txt",std::ios_base::out|std::ios_base::app);
+  fout << fixed;
+  fout.precision(6);
+  fout<<sixdof[0]<<" "<<sixdof[1]<<" "<<sixdof[2]<<" "<<sixdof[3]<<" "<<sixdof[4]<<" "<<sixdof[5]<<" "<<sixdof[6]<<endl; 
+  fout.close();
+  //printf("x=%f, y=%f, z=%f, w=%f, p=%f, q=%f, r=%f \n",sixdof[0],sixdof[1],sixdof[2],sixdof[3],sixdof[4],sixdof[5],sixdof[6]);
 }
 
 int main(int argc, char **argv)
