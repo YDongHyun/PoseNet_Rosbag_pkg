6f1310da98014941ed6c7810412684a6cfe29c7a
양동현 <80799025+YDongHyun@users.noreply.github.com>
2022-08-27 20:12:08 +0900

    Update datasetmaker.cpp

--- LeGO-LOAM/src/datasetmaker.cpp
+++ LeGO-LOAM/src/datasetmaker.cpp
@@ -2,12 +2,26 @@
 #include <fstream>
 #include <sensor_msgs/CompressedImage.h>
 #include <cv_bridge/cv_bridge.h>
+#include <string>
 
 float sixdof[7];
 std::ofstream fout;
+using namespace std;
+using std::string;
+using std::to_string;
+int global_cnt=0;
+int global_len_zero;
+string global_zeros;
 
-void sixdof_sub(const nav_msgs::Odometry::ConstPtr& laserOdometry2){
-  
+void Sixdof_sub(const nav_msgs::Odometry::ConstPtr& laserOdometry2){
+  global_cnt+=1;
+  global_zeros="";
+  string tmp=to_string(global_cnt);
+  int len=tmp.size();
+  global_len_zero=6-len;
+  for (int i=0;i<global_len_zero;i++){
+    global_zeros+="0";
+  }
   sixdof[0]= laserOdometry2->pose.pose.position.z;
   sixdof[1]= laserOdometry2->pose.pose.position.x;
   sixdof[2]= laserOdometry2->pose.pose.position.y;
@@ -18,7 +32,7 @@ void sixdof_sub(const nav_msgs::Odometry::ConstPtr& laserOdometry2){
   ofstream fout("train.txt",std::ios_base::out|std::ios_base::app);
   fout << fixed;
   fout.precision(6);
-  fout<<sixdof[0]<<" "<<sixdof[1]<<" "<<sixdof[2]<<" "<<sixdof[3]<<" "<<sixdof[4]<<" "<<sixdof[5]<<" "<<sixdof[6]<<endl; 
+  fout<<"seq/frame"<<global_zeros<<global_cnt<<" "<<sixdof[0]<<" "<<sixdof[1]<<" "<<sixdof[2]<<" "<<sixdof[3]<<" "<<sixdof[4]<<" "<<sixdof[5]<<" "<<sixdof[6]<<endl; 
   fout.close();
   //printf("x=%f, y=%f, z=%f, w=%f, p=%f, q=%f, r=%f \n",sixdof[0],sixdof[1],sixdof[2],sixdof[3],sixdof[4],sixdof[5],sixdof[6]);
 }
@@ -26,15 +40,19 @@ void sixdof_sub(const nav_msgs::Odometry::ConstPtr& laserOdometry2){
 void Image_sub(const sensor_msgs::CompressedImage::ConstPtr& CompressedImage){
   cv_bridge::CvImagePtr cvImg = cv_bridge::toCvCopy(CompressedImage, "bgr8");
   cv::Mat myOpenCVImg = cvImg->image;
-  cv::imwrite("test.png", myOpenCVImg);
+  string str ="seq/frame";
+  string png = ".png";
+  str+=global_zeros+to_string(global_cnt)+png;
+  cv::imwrite(str, myOpenCVImg);
 }
 
 int main(int argc, char **argv)
 {
+  //boost::filesystem::create_directory("/seq");
   ros::init(argc, argv, "listener");
   ros::NodeHandle n;
+  ros::Subscriber sub_sixdof = n.subscribe("/integrated_to_init", 1000, Sixdof_sub);
   ros::Subscriber sub_image = n.subscribe("/zed/left/image_rect_color/compressed", 1000, Image_sub);
-  ros::Subscriber sub_sixdof = n.subscribe("/integrated_to_init", 1000, sixdof_sub);
   ros::spin();
 
   return 0;
